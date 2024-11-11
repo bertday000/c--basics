fed98b9442ffec092c9b34c4d09bc6673f2e7922
bertday000 <shoniazuka06@gmail.com>
2024-11-11 20:49:17 +0400

    init

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("whats your name mortal");
+Console.ReadLine();
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine("whats your age");
+Console.ReadLine();
+Console.WriteLine("finished");  
